996a49cac3ed8b96c887f3c3fa916c800a12c8c7
Anzhela Ayvazyan <angela.ayvazyan17@gmail.com>
2016-10-26 17:37:57 +0400

    second commit; functionality: incomplete

--- script.js
+++ script.js
@@ -4,51 +4,38 @@
 function inputNumber(number) {
     var outputText = document.getElementById("input-area").value;
     document.getElementById("input-area").value = outputText + number;
-    switch (number) {
-        case  's+':
-            document.getElementById("input-area").value = outputText + "+";
-            break;
-        case 's-':
-            document.getElementById("input-area").value = outputText + "-";
-            break;
-        case 's*':
-            document.getElementById("input-area").value = outputText + "*";
-            break;
-        case 's/':
-            document.getElementById("input-area").value = outputText + "/";
-            break;
-
-        case 's=':
-            var splitNumbers;
-            var result;
-            for ( var i = 0; i < outputText.length; i++) {
-                switch (outputText[i]) {
-                    case '+':
-                    splitNumbers = document.getElementById("input-area").value.split("+");
-                    result = parseFloat(splitNumbers[0]) + parseFloat(splitNumbers[1]);
-                        break;
-
-                    case '-':
-                    splitNumbers = document.getElementById("input-area").value.split("-");
-                    result = parseFloat(splitNumbers[0]) - parseFloat(splitNumbers[1]);
-                        break;
-
-                    case  '*':
-                    splitNumbers = document.getElementById("input-area").value.split("*");
-                    result = parseFloat(splitNumbers[0]) * parseFloat(splitNumbers[1]);
-                break;
-                    case  '/':
-                    splitNumbers = document.getElementById("input-area").value.split("/");
-                    if (parseFloat(splitNumbers[1]) == 0) {
-                        result = "Error";
-                    }
-                    else
-                        result = parseFloat(splitNumbers[0]) / parseFloat(splitNumbers[1]);
-
-                break;
-            }
-            document.getElementById("input-area").value = result;
-
-    }
 }
+function inputSymbol(symbol) {
+    var outputText = document.getElementById("input-area").value;
+    document.getElementById("input-area").value = outputText + symbol;
+}
+function equality() {
+    var outputText = document.getElementById("input-area").value;
+    var splitNumbers;
+    var result;
+    for (var i = 0; i < outputText.length; i++) {
+        switch (outputText[i]) {
+            case'+':
+                splitNumbers = document.getElementById("input-area").value.split("+");
+                result = parseFloat(splitNumbers[0]) + parseFloat(splitNumbers[1]);
+                break;
+            case'-':
+                splitNumbers = document.getElementById("input-area").value.split("-");
+                result = parseFloat(splitNumbers[0]) - parseFloat(splitNumbers[1]);
+                break;
+            case'*':
+                splitNumbers = document.getElementById("input-area").value.split("*");
+                result = parseFloat(splitNumbers[0]) * parseFloat(splitNumbers[1]);
+                break;
+            case'/':
+                splitNumbers = document.getElementById("input-area").value.split("/");
+                if (parseFloat(splitNumbers[1]) == 0) {
+                    result = "Error";
+                }
+                else
+                    result = parseFloat(splitNumbers[0]) / parseFloat(splitNumbers[1]);
+                break;
+        }
+    }
+    document.getElementById("input-area").value = result;
 }
