93e601ff41e428beb48aa864fb3c00eb892122c4
Angela1997 <ayvazyan.angela@mail.ru>
2016-10-27 00:23:40 +0400

    fixed operations: optimization

--- script.js
+++ script.js
@@ -1,46 +1,40 @@
-var previousOperation = null;
-
 /**
  * Created by Home on 21.10.2016.
  */
+var operation;
 function inputNumber(number) {
-    var outputText = document.getElementById("input-area").value;
-    document.getElementById("input-area").value = outputText + number;
+  var outputText = document.getElementById("input-area").value;
+  document.getElementById("input-area").value = outputText + number;
 }
-
-function inputSymbol(currentOperation) {
-    var outputText = document.getElementById("input-area").value;
-    var splitNumbers;
-    var result;
-    if (previousOperation) {
-        splitNumbers = outputText.split(currentOperation);
-        var firstValue = parseFloat(splitNumbers[0]);
-        var secondValue = parseFloat(splitNumbers[1]);
-
-        switch (currentOperation) {
-            case'+':
-                result = firstValue + secondValue;
-                break;
-            case'-':
-                result = firstValue - secondValue;
-                break;
-            case'*':
-                result = firstValue * secondValue;
-                break;
-            case'/':
-                if (secondValue == 0) {
-                    result = "Error";
-                }
-                else
-                    result = firstValue / secondValue;
-                break;
+function inputSymbol(symbol) {
+  var outputText = document.getElementById("input-area").value;
+  var result;
+  if (symbol == '=') {
+    var splitNumbers = outputText.split(operation);
+    var firstValue = parseFloat(splitNumbers[0]);
+    var secondValue = parseFloat(splitNumbers[1]);
+    switch (operation) {
+      case '+':
+        result = firstValue + secondValue;
+        break;
+      case'-':
+        result = firstValue - secondValue;
+        break;
+      case '*':
+        result = firstValue * secondValue;
+        break;
+      case '/':
+        if (secondValue == 0) {
+          result = "Error";
         }
+        else
+          result = firstValue / secondValue;
+        break;
     }
     document.getElementById("input-area").value = result;
-
-    if (!previousOperation || previousOperation != '=') {
-        previousOperation = currentOperation;
-    } else {
-        previousOperation = null;
-    }
+  }
+  else {
+    operation = symbol;
+    document.getElementById("input-area").value = outputText + symbol;
+  }
 }
